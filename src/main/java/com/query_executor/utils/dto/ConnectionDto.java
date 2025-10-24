16f478f21977d9ca69b3104fb4baa532baf76991
Vishwajeet <Vishwajeet.singh@mayadataprivacy.eu>
2025-10-24 20:49:09 +0530

    gds

--- src/main/java/com/query_executor/utils/dto/ConnectionDto.java
+++ src/main/java/com/query_executor/utils/dto/ConnectionDto.java
@@ -5,6 +5,33 @@ import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.stereotype.*;
 
+/**
+ * Data Transfer Object (DTO) for database connection details.
+ * <p>
+ * This class encapsulates all necessary information required to establish a connection to a database.
+ * It is used to transfer connection parameters between client, controller, and service layers.
+ * <ul>
+ *     <li>host: The hostname or IP address of the database server.</li>
+ *     <li>port: The port number on which the database server is listening.</li>
+ *     <li>database: The name of the target database.</li>
+ *     <li>username: The username for database authentication.</li>
+ *     <li>password: The password for database authentication.</li>
+ *     <li>schema: The schema to use within the database (if applicable).</li>
+ *     <li>dbType: The type of the database (e.g., POSTGRES, MYSQL).</li>
+ * </ul>
+ *
+ * <b>Usage Example:</b>
+ * <pre>
+ *     ConnectionDto dto = new ConnectionDto();
+ *     dto.setHost("localhost");
+ *     dto.setPort(5432);
+ *     dto.setDatabase("mydb");
+ *     dto.setUsername("user");
+ *     dto.setPassword("pass");
+ *     dto.setSchema("public");
+ *     dto.setDbType("POSTGRES");
+ * </pre>
+ */
 @Data
 @Component
 public class ConnectionDto {
