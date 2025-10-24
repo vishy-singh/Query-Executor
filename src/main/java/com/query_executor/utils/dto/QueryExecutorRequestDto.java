60ae4e1038fb63d216b20b7840ee8b4704b46094
Vishwajeet <Vishwajeet.singh@mayadataprivacy.eu>
2025-10-24 20:49:35 +0530

    gdss

--- src/main/java/com/query_executor/utils/dto/QueryExecutorRequestDto.java
+++ src/main/java/com/query_executor/utils/dto/QueryExecutorRequestDto.java
@@ -6,7 +6,22 @@ import org.springframework.stereotype.*;
 
 import java.util.*;
 
-
+/**
+ * Data Transfer Object (DTO) for executing SQL queries.
+ * <p>
+ * This class encapsulates the information required to execute a query against a specific database connection.
+ * <ul>
+ *     <li>query: The SQL query string to be executed.</li>
+ *     <li>connectionId: The unique identifier of the database connection to use for execution.</li>
+ * </ul>
+ *
+ * <b>Usage Example:</b>
+ * <pre>
+ *     QueryExecutorRequestDto request = new QueryExecutorRequestDto();
+ *     request.setQuery("SELECT * FROM users");
+ *     request.setConnectionId(UUID.fromString("123e4567-e89b-12d3-a456-426614174000"));
+ * </pre>
+ */
 @Data
 @Component
 public class QueryExecutorRequestDto {
